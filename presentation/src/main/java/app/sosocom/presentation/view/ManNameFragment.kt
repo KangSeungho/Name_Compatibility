7a9e3796e7af6c7bf24bbdcb1dd3a7880d3357b6
KangSeungho <sosocom95@naver.com>
2022-03-24 13:56:33 +0900

    - Navigation 화면 이동 처리

--- presentation/src/main/java/app/sosocom/presentation/view/ManNameFragment.kt
+++ presentation/src/main/java/app/sosocom/presentation/view/ManNameFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import app.sosocom.presentation.R
 import app.sosocom.presentation.base.BaseFragment
 import app.sosocom.presentation.databinding.FragmentMainBinding
@@ -15,6 +16,10 @@ import app.sosocom.presentation.databinding.FragmentManNameBinding
  */
 class ManNameFragment : BaseFragment<FragmentManNameBinding>(R.layout.fragment_man_name) {
     override fun init() {
+        binding.fragment = this
+    }
 
+    fun nextBtnClick(view: View) {
+        findNavController().navigate(R.id.action_manNameFragment_to_resultFragment)
     }
 }
